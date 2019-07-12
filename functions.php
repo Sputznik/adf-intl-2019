b3981b2af0bf9eb95a52bc321e0cf8e9dded6296
unknown <samvthom16@gmail.com>
2019-07-12 14:59:06 +0530

    Fix styles for sub menu in primary navigation

--- functions.php
+++ functions.php
@@ -440,3 +440,15 @@ add_filter( 'excerpt_length', function(){
 }, 999 );
 
 include("inc/orbit-inc.php");
+
+class WPSE_78121_Sublevel_Walker extends Walker_Nav_Menu
+{
+    function start_lvl( &$output, $depth = 0, $args = array() ) {
+        $indent = str_repeat("\t", $depth);
+        $output .= "\n$indent<div class='sub-menu-wrap'><ul class='sub-menu'>\n";
+    }
+    function end_lvl( &$output, $depth = 0, $args = array() ) {
+        $indent = str_repeat("\t", $depth);
+        $output .= "$indent</ul></div>\n";
+    }
+}
