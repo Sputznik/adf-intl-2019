d403561aca7bc8af04c8f4cb651fc703102d47ff
unknown <samvthom16@gmail.com>
2019-07-16 11:17:26 +0530

    Updating coontainer screen sizes

--- archive.php
+++ archive.php
@@ -12,27 +12,25 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 			<div class="archive-articles gray-back">
-				<div class="wrap">
-		<?php
-		if ( have_posts() ) : ?>
-			<header class="page-header">
+				<div class="container">
+				<?php if ( have_posts() ) : ?>
+					<header class="page-header">
+					<?php
+						the_archive_title( '<h1 class="page-title">', '</h1>' );
+						//the_archive_description( '<div class="archive-description">', '</div>' );
+					?>
+					</header><!-- .page-header -->
+					<ul class="article-list three-list group">
+						<?php while ( have_posts() ) : the_post(); ?>
+						<li><?php get_template_part( 'template-parts/content', 'article' );?></li>
+						<?php endwhile; wp_reset_postdata();?>
+					</ul>
+					<?php the_posts_navigation(array('prev_text' => __( 'More &raquo;' ), 'next_text' => __( '&laquo; Previous' ),));?>
 				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					//the_archive_description( '<div class="archive-description">', '</div>' );
+					else :
+						get_template_part( 'template-parts/content', 'none' );
+					endif;
 				?>
-			</header><!-- .page-header -->
-			<ul class="article-list three-list group">
-				<?php while ( have_posts() ) : the_post(); ?>
-				<li><?php get_template_part( 'template-parts/content', 'article' );?></li>
-				<?php endwhile; wp_reset_postdata();?>
-			</ul>
-			<?php the_posts_navigation(array('prev_text' => __( 'More &raquo;' ), 'next_text' => __( '&laquo; Previous' ),));
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
 				</div>
 			</div>
 		</main><!-- #main -->
