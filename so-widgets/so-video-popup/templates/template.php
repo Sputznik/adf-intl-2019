23a03f63b87c27c63325a07e9514dbec4e0b2d84
Steve <anil.stephen66@gmail.com>
2019-07-19 12:27:41 +0530

    Updated so-video-popup styles for small screens

--- so-widgets/so-video-popup/templates/template.php
+++ so-widgets/so-video-popup/templates/template.php
@@ -1,20 +1,12 @@
+<?php $video_thumb =  wp_get_attachment_url( $instance['video_thumb'] );?>
 <div class='video-popup'>
-  <?php
-  foreach ( $instance['video_repeater'] as $key => $value) {
-    $video_thumb =  wp_get_attachment_url( $value['video_thumb'] );
-  ?>
   <div class='video'>
-    <img src="<?php _e( $video_thumb ); ?>" />
+    <img src="<?php ( $video_thumb ) ? _e( $video_thumb ) : _e( $instance['video_thumb_fallback'] ) ?>"/>
   </div>
   <div class='overlay-text'>
-    <a href="<?php _e( $value['video_link'] ); ?>" rel="wp-video-lightbox">
-      <h3>
-        <?php _e( $value['video_title'] ); ?>
-      </h3>
-      <p>
-        <?php _e( $value['video_desc'] );?>
-      </p>
+    <a href="<?php _e( $instance['video_link'] ); ?>" rel="wp-video-lightbox">
+      <h3><?php _e( $instance['video_title'] ); ?></h3>
+      <p><?php _e( $instance['video_desc'] );?></p>
     </a>
   </div>
 </div>
-<?php } ?>
