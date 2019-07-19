f9e5abdd4e01d0d75bd27cef832442ac3315c5b2
unknown <samvthom16@gmail.com>
2019-07-19 10:19:01 +0530

    Updates to hero image widget

--- so-widgets/so-header-with-card/templates/template.php
+++ so-widgets/so-header-with-card/templates/template.php
@@ -5,20 +5,22 @@
   $widget_id = 'sow-bg-'.$sp_sow->getUniqueID( $instance );
 
   $image_src =  wp_get_attachment_url( $instance['header_image'] );
-  $card_bg = $instance['card_bgcolor']."CC";
+  $card_bg = $instance['card']['bg']."CC";
+
 
 ?>
 <div id=<?php _e( $widget_id );?> class="sow-bg-image" style="">
   <div class="container">
-    <div class="header-card" style="background-color:<?php _e( $card_bg ); ?>";>
-      <?php _e( $instance['card_txt'] );?>
-      <?php if( $instance['btn_url'] ) :?>
-        <a type="button" href="<?php _e( $btn_url );?>"class="btn"><?php _e( $instance['btn_text'] ); ?></a>
+    <?php if( $instance['card']['html'] ):?>
+    <div class="header-card" style="background-color:<?php _e( $card_bg ); ?>; color: <?php _e( $instance['card']['color']);?>;">
+      <?php _e( $instance['card']['html'] );?>
+      <?php if( $instance['btn']['url'] ) :?>
+        <a type="button" style="border-color:<?php _e( $instance['card']['color'] );?>;color:<?php _e( $instance['card']['color'] );?>;" href="<?php _e( $instance['btn']['url'] );?>"class="btn"><?php _e( $instance['btn']['text'] ); ?></a>
       <?php endif;?>
     </div>
+  <?php endif;?>
   </div>
 </div>
-
 <style>
   <?php _e( "#".$widget_id );?>{
     background-image: url( '<?php _e( $image_src );?>' );
