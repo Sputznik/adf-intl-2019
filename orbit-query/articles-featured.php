52d3e8edbd32830195c1f5ed163a001ee4280be6
unknown <samvthom16@gmail.com>
2019-07-22 17:10:25 +0530

    test

--- orbit-query/articles-featured.php
+++ orbit-query/articles-featured.php
@@ -2,6 +2,7 @@
 <?php while( $this->query->have_posts() ) : $this->query->the_post();?>
   <?php global $post; $type = get_post_type($post); ?>
   <div class="secondary-feature group <?php if($type == 'resource'){echo 'resource-feature';} ?>">
-	   <?php get_template_part( 'template-parts/content', 'featured' );?>
+    <?php the_title();?>
+	  <?php //get_template_part( 'template-parts/content', 'featured' );?>
   </div>
 <?php endwhile;?>
