da07367789e165d4faf0526c0e7502d0b988104b
unknown <samvthom16@gmail.com>
2019-07-22 17:07:42 +0530

    test

--- orbit-query/articles-grid3.php
+++ orbit-query/articles-grid3.php
@@ -1,5 +1,5 @@
 <ul class="article-list three-list group">
   <?php while( $this->query->have_posts() ) : $this->query->the_post();?>
-	<li><?php get_template_part( 'template-parts/content', 'article' );?></li>
+	<li><?php //get_template_part( 'template-parts/content', 'article' );?></li>
   <?php endwhile;?>
 </ul>
