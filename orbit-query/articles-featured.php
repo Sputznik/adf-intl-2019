4f21c70d3f3f63e325366dc5ecd6d1ba67c9ecd9
unknown <samvthom16@gmail.com>
2019-07-22 17:13:04 +0530

    test

--- orbit-query/articles-featured.php
+++ orbit-query/articles-featured.php
@@ -2,7 +2,6 @@
 <?php while( $this->query->have_posts() ) : $this->query->the_post();?>
   <?php global $post; $type = get_post_type($post); ?>
   <div class="secondary-feature group <?php if($type == 'resource'){echo 'resource-feature';} ?>">
-    <?php the_title();?>
-	  <?php //get_template_part( 'template-parts/content', 'featured' );?>
+    <?php get_template_part( 'template-parts/content', 'featured' );?>
   </div>
 <?php endwhile;?>
--- template-parts/content-featured.php
+++ template-parts/content-featured.php
@@ -1,18 +1,10 @@
+<?php
+  the_title();
+/*
+
 <?php global $post; $type = get_post_type($post); ?>
 <a href = "<?php the_permalink(); ?>" class="feature-image">
-  <?php
-    if ( has_post_thumbnail() ) { the_post_thumbnail(); }
-  /*
-  <?php if($type == 'resource'): ?>
-  <img src="<?php echo wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'large' )[0]; ?>">
-  <?php else : ?>
-  <picture>
-    <source srcset="<?php echo wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'square' )[0]; ?>" media="(max-width: 44em)">
-    <source srcset="<?php echo wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'large' )[0]; ?>" media="(max-width: 62.5em)">
-    <?php echo wp_get_attachment_image(get_post_thumbnail_id( get_the_ID() ), 'square'); ?>
-  </picture>
-  <?php endif; ?>
-  */ ?>
+  <?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
   <span class="content-type"><?php echo get_post_type($post); ?></span>
 </a>
 <div class="feature-content">
@@ -22,3 +14,5 @@
     <strong class="link">Published on <?php echo get_the_date();?></strong>
   </a>
 </div>
+
+*/
