67854cd2a84a9e692a971d84fd815d7d984e911a
unknown <samvthom16@gmail.com>
2019-07-22 17:14:48 +0530

    test

--- template-parts/content-featured.php
+++ template-parts/content-featured.php
@@ -1,7 +1,7 @@
 <?php
-  the_title();
-/*
+  //the_title();
 
+?>
 <?php global $post; $type = get_post_type($post); ?>
 <a href = "<?php the_permalink(); ?>" class="feature-image">
   <?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
@@ -10,9 +10,7 @@
 <div class="feature-content">
   <a href="<?php the_permalink(); ?>">
     <h2 class="no-margin-top"><?php the_title(); ?></h2>
-    <p class="text-muted narrow-text"><?php echo excerpt(20); ?></p>
+    <p class="text-muted narrow-text"><?php //echo excerpt(20); ?></p>
     <strong class="link">Published on <?php echo get_the_date();?></strong>
   </a>
 </div>
-
-*/
