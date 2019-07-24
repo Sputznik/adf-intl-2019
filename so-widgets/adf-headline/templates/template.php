f845751239ae4e4e4c4a09575e4e543fccf417a2
unknown <samvthom16@gmail.com>
2019-07-24 16:04:32 +0530

    Container class within headline

--- so-widgets/adf-headline/templates/template.php
+++ so-widgets/adf-headline/templates/template.php
@@ -4,9 +4,11 @@
 
 
 ?>
-<div class="adf-sow-headline text-center <?php _e( $theme_class );?>">
-  <?php if( isset( $instance['heading'] ) && $instance['heading'] ):?><h1><?php _e( $instance['heading'] );?></h1><?php endif;?>
-  <?php if( isset( $instance['subheading'] ) && $instance['subheading'] ):?><h4><?php _e( $instance['subheading'] );?></h4><?php endif;?>
-  <?php if( isset( $instance['hide_decoration'] ) && !$instance['hide_decoration'] ):?><div class="decoration"></div><?php endif;?>
-  <div class='text'><?php _e( $instance['text'] );?></div>
+<div class="container">
+  <div class="adf-sow-headline text-center <?php _e( $theme_class );?>">
+    <?php if( isset( $instance['heading'] ) && $instance['heading'] ):?><h1><?php _e( $instance['heading'] );?></h1><?php endif;?>
+    <?php if( isset( $instance['subheading'] ) && $instance['subheading'] ):?><h4><?php _e( $instance['subheading'] );?></h4><?php endif;?>
+    <?php if( isset( $instance['hide_decoration'] ) && !$instance['hide_decoration'] ):?><div class="decoration"></div><?php endif;?>
+    <div class='text'><?php _e( $instance['text'] );?></div>
+  </div>
 </div>
