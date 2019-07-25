0d2d593410fe9ce86942412ed94b9b962c0360cd
unknown <samvthom16@gmail.com>
2019-07-25 10:34:53 +0530

    Single staff template update

--- single-advocacy-team.php
+++ single-advocacy-team.php
@@ -21,28 +21,44 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<div class="page-content">
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<div class="page-content">
 					<?php while ( have_posts() ) : the_post();?>
-						<div class='row'>
-							<div class='container col-13'>
-								<div class='col'><?php if ( has_post_thumbnail() ) { the_post_thumbnail('medium'); }?></div>
-								<div class='col'>
+					<div class="row gray" style="padding:100px 0"></div>
+					<div class='row' style="margin-top:-200px;">
+						<div class='container col-13'>
+							<div class='col'><?php if ( has_post_thumbnail() ) { the_post_thumbnail('medium'); }?></div>
+							<div class='col'>
+								<div class="staff-header">
 									<h5 class='bold text-muted uppercase no-margin-top'><?php _e( do_shortcode('[orbit_terms taxonomy="department" link="0"]') );?></h5>
 									<h1 class='no-margin'><?php the_title();?></h1>
-									<h5 class='uppercase text-muted <?php if( $staff_description ):?>border-bottom<?php endif;?>'><?php _e( do_shortcode('[orbit_cf id="team-position"]') );?></h5>
-									<div class='excerpt text-muted'><?php the_excerpt();?></div>
 								</div>
+								<h5 class='uppercase text-muted <?php if( $staff_description ):?>border-bottom<?php endif;?>'><?php _e( do_shortcode('[orbit_cf id="team-position"]') );?></h5>
+								<div class='excerpt text-muted'><?php the_excerpt();?></div>
+								<a class="sp-btn" href="#modal-interview" data-toggle="modal">Request An Interview</a>
+								<?php
+									global $sp_sow;
+									$sp_sow->modal( 'modal-interview', 'Gravity form will come here' );
+								?>
 							</div>
 						</div>
-						<?php if( $staff_description ): ?>
-						<div class='row gray'><div class='container'><?php the_content();?></div></div>
-						<?php endif;?>
-					<?php endwhile;?>
 					</div>
-				</article>
+					<?php if( $staff_description ): ?>
+					<div class='row gray'><div class='container'><?php the_content();?></div></div>
+					<?php endif;?>
+					<?php endwhile;?>
+				</div>
+			</article>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php get_footer();?>
+
+<style>
+@media( min-width: 769px ){
+	.staff-header{
+		min-height: 140px;
+	}
+}
+a[href].sp-btn{ margin-top: 20px; display: inline-block;}
+</style>
