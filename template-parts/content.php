a716a852ed62edff4b773aed744b3b12e6f949fb
unknown <samvthom16@gmail.com>
2019-07-25 16:21:58 +0530

    Bugfix

--- template-parts/content.php
+++ template-parts/content.php
@@ -333,7 +333,7 @@ $type = get_post_type($post->ID);
 		?>
 		<div class="home-articles <?php the_sub_field('feat_back'); ?>" id="section<?php echo $i; ?>">
 			<div class="wrap">
-				<?php get_template_part( 'template-p0arts/content', 'featured' );?>
+				<?php get_template_part( 'template-parts/content', 'featured' );?>
 			</div>
 		</div>
 		<?php wp_reset_postdata(); endif; ?>
