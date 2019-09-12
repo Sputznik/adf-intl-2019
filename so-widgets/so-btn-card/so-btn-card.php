ffa665b27cf6dd94fb279f272425f2675cedadab
Steve <anil.stephen66@gmail.com>
2019-09-12 15:54:38 +0530

    Added card widget with button

---
+++ so-widgets/so-btn-card/so-btn-card.php
@@ -0,0 +1,69 @@
+<?php
+/*
+Widget Name: ADF Card Widget with Button
+Description: Card widget with button
+Author: Stephen Anil, Sputznik
+Author URI: http://www.sputznik.com
+Widget URI:
+Video URI:
+*/
+class SP_BUTTON_CARD extends SiteOrigin_Widget{
+  function __construct(){
+    $form_options = array(
+      'card_btn_repeater' => array(
+        'type'      => 'repeater',
+        'label'     => 'Card Repeater',
+        'item_name' =>  __( 'Add Card', 'siteorigin-widgets' ),
+        'fields'    =>  array(
+          'card_icon' => array(
+            'type' => 'icon',
+            'label' => __( 'Select an icon', 'siteorigin-widgets' ),
+          ),
+          'heading_txt'  =>  array(
+            'type'  =>  'text',
+            'label' =>  __( 'Heading', 'siteorigin-widgets' ),
+            'default' =>  '',
+          ),
+          'card_desc'     => array(
+            'type'  =>  'text',
+            'label' =>  __( 'Small Description', 'siteorigin-widgets' ),
+            'default' => ''
+          ),
+          'card_btn_txt'  =>  array(
+            'type'      => 'text',
+            'label'     => __( 'Button Text', 'siteorigin-widgets' ),
+            'default'   => ''
+          ),
+          'card_link'  =>  array(
+            'type'      => 'link',
+            'label'     => __( 'Link', 'siteorigin-widgets' ),
+            'default'   => ''
+          ),
+        )
+      ),
+    );
+    parent::__construct(
+      'so-btn-card',
+      __( 'ADF Card Widget with Button ','siteorigin-widgets'),
+      array(
+        'description' =>  __( 'Card Widget with Button','siteorigin-widgets' ),
+        'help'        =>  ''
+      ),
+      array(),
+      $form_options,
+      plugin_dir_path(__FILE__).'/so-widgets/so-btn-card'
+    );
+  }//construct function ends here
+  function get_template_name($instance){
+    return 'template';
+  }
+  function get_template_dir($instance) {
+    return 'templates';
+  }
+  function get_style_name($instance){
+    return '';
+    }
+}
+siteorigin_widget_register('so-btn-card',__FILE__,'SP_BUTTON_CARD');
+
+  ?>
---
+++ so-widgets/so-btn-card/templates/template.php
@@ -0,0 +1,15 @@
+<div class="btn-card-container">
+  <?php foreach ( $instance['card_btn_repeater'] as $key => $value) :?>
+  <?php $card_icon = siteorigin_widget_get_icon( $value['card_icon'], array( '' ) ); ?>
+  <div class="btn-card">
+    <div class="btn-card-body">
+      <div class="btn-card-icon"><?php echo $card_icon;?></div>
+      <div class="btn-card-title"><h4 class="uppercase text-center"><?php _e( $value['heading_txt'] );?></h4></div>
+      <div class="btn-card-content"><p class="text-center"><?php _e( $value['card_desc'] );?></p></div>
+      <div class="btn-card-btn">
+        <?php ( $value['card_link'] ) ? _e( '<a class="" href="'.$value['card_link'].'";>'.$value['card_btn_txt'].'</a>' ) : '' ?>
+      </div>
+    </div>
+  </div>
+  <?php endforeach; ?>
+</div><!-- .btn-card-container -->
