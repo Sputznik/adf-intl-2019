0f0fc4c3a5feb6c67e8186ffd68b4aa9c4784a8d
unknown <samvthom16@gmail.com>
2019-09-13 11:19:01 +0530

    Border for image grids

--- orbit-query/articles-img-grid.php
+++ orbit-query/articles-img-grid.php
@@ -1,4 +1,4 @@
-<ul class="article-list three-list group space">
+<ul class="article-list three-list group space article-images">
   <?php while( $this->query->have_posts() ) : $this->query->the_post();?>
 	<li><?php get_template_part( 'template-parts/content', 'img-grid' );?></li>
   <?php endwhile;?>
