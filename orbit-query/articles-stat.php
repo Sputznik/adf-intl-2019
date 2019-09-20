546888fd05f6e4230c174fe6e1891b87c923fd0e
Steve <anil.stephen66@gmail.com>
2019-09-20 15:09:35 +0530

    Added a wrapper for articles-stat thumbnail

--- orbit-query/articles-stat.php
+++ orbit-query/articles-stat.php
@@ -1,7 +1,9 @@
 <ul class='list-inline orbit-stat three-list'>
   <?php while( $this->query->have_posts() ) : $this->query->the_post();?>
   <li class="orbit-article">
-    <?php _e( do_shortcode('[orbit_thumbnail]') );?>
+    <div class="orbit-article-thumbnail">
+      <?php _e( do_shortcode('[orbit_thumbnail]') );?>
+    </div>
     <div class='orbit-excerpt'><?php the_title();?></div>
     <?php get_template_part( 'template-parts/share', 'socialmedia' );?>
   </li>
