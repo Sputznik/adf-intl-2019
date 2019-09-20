11ef88da5f46b06c1128a2d44c74d0145ce607e1
Steve <anil.stephen66@gmail.com>
2019-09-20 13:41:14 +0530

    Updated social-media link parameters

--- orbit-query/articles-stat.php
+++ orbit-query/articles-stat.php
@@ -2,13 +2,13 @@
   <?php while( $this->query->have_posts() ) : $this->query->the_post();?>
   <li class="orbit-article">
     <?php _e( do_shortcode('[orbit_thumbnail]') );?>
-    <div class='orbit-excerpt'><?php the_excerpt();?></div>
+    <div class='orbit-excerpt'><?php the_title();?></div>
     <?php get_template_part( 'template-parts/share', 'socialmedia' );?>
   </li>
   <?php endwhile;?>
 </ul>
 <style>
-  
+
   /*
   .orbit-stat{
     display: grid;
--- template-parts/share-socialmedia.php
+++ template-parts/share-socialmedia.php
@@ -1,3 +1,4 @@
+<?php global $post;?>
 <ul class="list-inline social-icons">
   <li>
     <a target="_blank" href="https://www.facebook.com/sharer.php?u=<?php the_permalink();?>">
@@ -5,12 +6,12 @@
     </a>
   </li>
   <li>
-    <a target="_blank" href="https://twitter.com/intent/tweet?url=<?php the_permalink();?>">
+    <a target="_blank" href="https://twitter.com/intent/tweet?text=<?php echo $post->post_excerpt;?>&url=<?php the_permalink();?>&via=ADFIntl">
       <img src="<?php bloginfo('template_url');?>/_i/twitter.png" />
     </a>
   </li>
   <li>
-    <a target="_blank" href="https://www.linkedin.com/sharing/share-offsite/?url=<?php the_permalink();?>">
+    <a target="_blank" href="https://www.linkedin.com/sharing/share-offsite/?url=<?php the_permalink(); ?>">
       <img src="<?php bloginfo('template_url');?>/_i/linkedin.png" />
     </a>
   </li>
