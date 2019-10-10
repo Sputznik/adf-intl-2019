de53d6c74d850df55c2ca21df6e0f423ec85b169
unknown <samvthom16@gmail.com>
2019-10-10 09:53:36 +0530

    Update social media icons to fa icons

--- template-parts/share-socialmedia.php
+++ template-parts/share-socialmedia.php
@@ -1,4 +1,6 @@
 <?php global $post;?>
+
+<?php /*
 <ul class="list-inline social-icons">
   <li>
     <a target="_blank" href="https://www.facebook.com/sharer.php?u=<?php the_permalink();?>">
@@ -16,3 +18,37 @@
     </a>
   </li>
 </ul>
+*/ ?>
+<?php
+
+  $permalink = get_the_permalink();
+
+  $icons = array(
+    'fb'  => array(
+      'icon'  => 'fa fa-facebook',
+      'url'   => "https://www.facebook.com/sharer.php?u=$permalink"
+    ),
+    'tw'  => array(
+      'icon'  => 'fa fa-twitter',
+      'url'   => "https://twitter.com/intent/tweet?text=".$post->post_excerpt."&url=$permalink&via=ADFIntl"
+    ),
+    'li'  => array(
+      'icon'  => 'fa fa-linkedin',
+      'url'   => "https://www.linkedin.com/sharing/share-offsite/?url=$permalink"
+    ),
+  );
+
+  _e('<ul class="list-inline social-icons">');
+  foreach( $icons as $key => $icon ){
+
+    $class = $key." social-fa-icon";
+
+    _e("<li>");
+    _e("<a class='".$class."' target='_blank' href='" . $icon['url'] . "'>");
+    _e("<i class='".$icon['icon']."'></i>");
+    _e("</a>");
+    _e("<li>");
+  }
+  _e('</ul>');
+
+?>
