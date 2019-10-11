7b36ed567612c34cb5aed08ee7883df5771a38fd
unknown <samvthom16@gmail.com>
2019-10-11 09:22:42 +0530

    Overlay text for statistics

--- orbit-query/articles-stat.php
+++ orbit-query/articles-stat.php
@@ -1,10 +1,10 @@
 <ul class='list-inline orbit-stat three-list'>
   <?php while( $this->query->have_posts() ) : $this->query->the_post();?>
   <li class="orbit-article">
-    <div class="orbit-article-thumbnail">
+    <div class="orbit-article-thumbnail overlay-text-parent">
       <?php _e( do_shortcode('[orbit_thumbnail]') );?>
+      <div class='orbit-excerpt overlay-text'><?php the_title();?></div>
     </div>
-    <div class='orbit-excerpt'><?php the_title();?></div>
     <?php get_template_part( 'template-parts/share', 'socialmedia' );?>
   </li>
   <?php endwhile;?>
