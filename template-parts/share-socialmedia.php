718e541e2a83f823d0da3865d998e4f4a096a577
unknown <samvthom16@gmail.com>
2019-10-11 09:54:29 +0530

    Download icon for statistics

--- template-parts/share-socialmedia.php
+++ template-parts/share-socialmedia.php
@@ -48,9 +48,9 @@
   <ul class="list-inline social-icons">
   <?php foreach( $icons as $key => $icon ): $class = $key." social-fa-icon"; ?>
     <li>
-      <a class='<?php _e( $class ); ?>' target='_blank' href='<?php _e( $icon['url'] );?>' <?php if( $key == 'download' ){_e("download")}?>>
+      <a class='<?php _e( $class ); ?>' target='_blank' href='<?php _e( $icon['url'] );?>'>
         <i class='<?php _e( $icon['icon'] );?>'></i>
       </a>
     <li>
-  }
+  <?php endforeach;?>
   </ul>
