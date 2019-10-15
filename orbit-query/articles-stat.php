1c892871823cb7c67052c27474f18e06d991d846
unknown <samvthom16@gmail.com>
2019-10-15 10:36:14 +0530

    Update orbit stats to slick

--- orbit-query/articles-stat.php
+++ orbit-query/articles-stat.php
@@ -24,17 +24,18 @@
   );
 
 ?>
-
-<div data-behaviour="slick" class="test-slick" data-slick='<?php _e( wp_json_encode( $slick ) );?>'>
-  <?php while( $this->query->have_posts() ) : $this->query->the_post();?>
-  <div>
-    <div class="article">
-      <div class="orbit-article-thumbnail overlay-text-parent">
-        <?php _e( do_shortcode('[orbit_thumbnail]') );?>
-        <div class='orbit-excerpt overlay-text'><?php the_title();?></div>
+<div class="container">
+  <div data-behaviour="slick" class="test-slick" data-slick='<?php _e( wp_json_encode( $slick ) );?>'>
+    <?php while( $this->query->have_posts() ) : $this->query->the_post();?>
+    <div>
+      <div class="article">
+        <div class="orbit-article-thumbnail overlay-text-parent">
+          <?php _e( do_shortcode('[orbit_thumbnail]') );?>
+          <div class='orbit-excerpt overlay-text'><?php the_title();?></div>
+        </div>
+        <?php get_template_part( 'template-parts/share', 'socialmedia' );?>
       </div>
-      <?php get_template_part( 'template-parts/share', 'socialmedia' );?>
     </div>
+    <?php endwhile;?>
   </div>
-  <?php endwhile;?>
 </div>
