94e3bfbd8bcc1f86e43391d9ae4b46ea06bbdaef
Steve <anil.stephen66@gmail.com>
2019-11-19 12:11:45 +0530

    Updated orbit-img-grid template

--- template-parts/content-img-grid.php
+++ template-parts/content-img-grid.php
@@ -1,4 +1,5 @@
 <?php
+  $post_type = get_post_type();
   $thumb = wp_get_attachment_thumb_url( get_the_ID() );
   $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' )[0];
 ?>
@@ -6,6 +7,8 @@
   <div class='article-featured-image' style="background-image: url( <?php _e( $image );?> );" ></div>
   <div class="article-content">
     <h4><?php echo get_the_title(); ?></h4>
+    <?php if($post_type != 'campaign') : ?>
     <strong class="link">Published on <?php echo get_the_date( 'j M Y' );?></strong>
+    <?php endif;?>
   </div>
 </a>
