626c486192effb3509cfac4116dbc3db9f71ffe2
unknown <samvthom16@gmail.com>
2019-11-28 11:20:11 +0530

    Team template without permalinks

--- orbit-query/articles-team-without-link.php
+++ orbit-query/articles-team-without-link.php
@@ -1,5 +1,5 @@
 <div class="col-4 team-list">
   <?php while( $this->query->have_posts() ) : $this->query->the_post();?>
-	<?php get_template_part( 'template-parts/content', 'team-without-link' );?>
+	<?php get_template_part( 'template-parts/content', 'team-without-links' );?>
   <?php endwhile;?>
 </div>
