48cb5c70d66962bed5b442c2f58ab8382e74f60c
unknown <samvthom16@gmail.com>
2019-12-05 14:42:43 +0530

    Read more text in search template

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -39,7 +39,7 @@
 		<div class="feature-content">
 			<h3><?php the_title(); ?></h3>
 			<p class="narrow-text"><?php echo excerpt(30); ?></p>
-			<a href = "<?php the_permalink(); ?>">READ MORE</a>
+			<a href = "<?php the_permalink(); ?>"><?php _e( 'READ MORE', 'adf-intl' );?></a>
 		</div>
 	</div>
 </article><!-- #post-<?php the_ID(); ?> -->
