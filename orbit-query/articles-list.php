379b4a31950dc541fe57f2b76c72b7b8ec186a8f
unknown <samvthom16@gmail.com>
2019-12-05 17:21:20 +0530

    Fix pagination for articles list

--- orbit-query/articles-list.php
+++ orbit-query/articles-list.php
@@ -1,5 +1,5 @@
-<ul class="orbit-article-list wrap list-unstyled">
+<ul id="<?php _e( $atts['id'] );?>" data-target="<?php _e('li.orbit-article');?>" data-url="<?php _e( $atts['url'] );?>" class="orbit-article-list wrap list-unstyled">
   <?php while( $this->query->have_posts() ) : $this->query->the_post();?>
-	<li><?php get_template_part( 'template-parts/content', 'search' );?></li>
+	<li class="orbit-article"><?php get_template_part( 'template-parts/content', 'search' );?></li>
   <?php endwhile;?>
 </ul>
