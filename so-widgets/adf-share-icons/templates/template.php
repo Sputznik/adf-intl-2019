3aa6c7842051a2b573bea2f4c55a4c3a1344a9a6
Steve <anil.stephen66@gmail.com>
2020-03-12 14:26:50 +0530

    Updated adf-share-icons

--- so-widgets/adf-share-icons/templates/template.php
+++ so-widgets/adf-share-icons/templates/template.php
@@ -1,7 +1,9 @@
 <!-- ADF SHARE ICONS WIDGET TEMPLATE -->
 <div class="entry-content sm-wrap">
   <?php
-    echo "<h5 class='social-media-heading uppercase'><span>Share</span></h5>";
+    echo "<h5 class='social-media-heading uppercase'><span>";
+    _e( 'Share', 'adf-intl');
+    echo "</span></h5>";
     get_template_part( 'template-parts/share', 'socialmedia' );
   ?>
 </div>
